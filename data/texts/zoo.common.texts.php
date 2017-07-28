fe79723f10c634b0d279d73ec1743a83e43333a6
Ralf Eggert <ralf@travello.com>
2017-07-28 20:54:43 +0200

    Added cancel texts and animal keys

--- data/texts/zoo.common.texts.php
+++ data/texts/zoo.common.texts.php
@@ -14,6 +14,8 @@ return [
     'alexaRepromptMessage' => 'Soll ich noch ein Tier nennen oder willst du abbrechen',
     'alexaHelpTitle'       => 'Hilfe',
     'alexaHelpMessage'     => 'Du kannst mich fragen, welches Tier noch in deinem Zoo ist und wie viele Tiere in deinem Zoo sind.',
+    'alexaCancelTitle'     => 'Danke',
+    'alexaCancelMessage'   => 'Danke und bis bald!',
     'alexaStopTitle'       => 'Danke',
     'alexaStopMessage'     => 'Danke und bis bald!',
 ];
