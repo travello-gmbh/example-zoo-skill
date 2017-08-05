08bca08c5d45941538aabea9558e294e80bdf6b2
Ralf Eggert <ralf@travello.com>
2017-08-05 21:23:15 +0200

    Updates for release 1.1.0

--- module/Zoo/src/Application/ZooApplication.php
+++ module/Zoo/src/Application/ZooApplication.php
@@ -54,6 +54,13 @@ class ZooApplication extends AbstractAlexaApplication
     }
 
     /**
+     * Reset the session attributes
+     */
+    protected function resetSessionAttributes()
+    {
+    }
+
+    /**
      * Handle custom application intents
      *
      * @return bool
